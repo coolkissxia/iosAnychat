1eed2f15fe44ee7fddad58fce158915f2994ff81
AzeaelCool <coolkissxia@163.com>
2018-05-03 18:17:59 +0800

    Initial commit

---
+++ MyApp/Classes/AnyChatVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnyChatVideoViewController.h
+//  MyApp
+//
+//  Created by 刘佳杰 on 2018/4/28.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnyChatVideoViewController : UIViewController
+
+@end
---
+++ MyApp/Classes/AnyChatVideoViewController.m
@@ -0,0 +1,37 @@
+//
+//  AnyChatVideoViewController.m
+//  MyApp
+//
+//  Created by 刘佳杰 on 2018/4/28.
+//
+//
+
+#import "AnyChatVideoViewController.h"
+
+@interface AnyChatVideoViewController ()
+
+@end
+
+@implementation AnyChatVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
